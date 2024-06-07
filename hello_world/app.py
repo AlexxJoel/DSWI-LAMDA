5fbe969d5f79d94ebfb5d2c57903e25db22da337
Joel Alejandro Herrera Hernandez <105768917+AlexxJoel@users.noreply.github.com>
2024-06-07 01:22:42 -0600

    Update app.py

--- hello_world/app.py
+++ hello_world/app.py
@@ -39,7 +39,7 @@ def lambda_handler(event, context):
             "fullname": "Herrera Hernandez Joel Alejandro",
             "grade": "9",
             "group": "A",
-            "message": "hello world - ",
+            "message": "hello world - jfj ",
             # "location": ip.text.replace("\n", "")
         }),
     }
